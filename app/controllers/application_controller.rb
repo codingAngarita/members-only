12e8d78c3285c7dcd3ec8d7d45bc17d3ba2c6138
Carlos Sol <fersolmarin@hotmail.com>
2019-07-30 17:17:55 -0500

    #login & #generateToken in Action Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  def login
+    cookies.permanent[:remember_token] = generateToken
+  end
+
+  private def generateToken
+    token = SecureRandom.urlsafe_base64
+    token = Digest::SHA1.hexdigest(token)
+    token = token.to_s
+  end
 end
