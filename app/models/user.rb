b8a9cf0523227cda743c4b1e7a999cce17d0f6f5
Carlos Sol <fersolmarin@hotmail.com>
2019-07-30 16:44:55 -0500

    Generates And Saves Secure Session Token for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  require 'digest'
+  
+  before_create :generateToken
 
   has_secure_password
+
+  private def generateToken
+    token = SecureRandom.urlsafe_base64
+    token = Digest::SHA1.hexdigest(token)
+    token = token.to_s
+    self.remember_token = token
+  end
 end
