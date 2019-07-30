62a004e4a9ea81f4ef5544f48e93f8c2198f47e5
Carlos Sol <fersolmarin@hotmail.com>
2019-07-30 17:06:25 -0500

    Authenticates New Users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,14 @@
 class SessionsController < ApplicationController
-
   def new
+    @user = User.new
   end
   
+  def create
+    user = User.find_by(email: params[:user][:email].downcase)
+    if user && user.authenticate(params[:user][:password])
+      # Sends Somewhere
+    else
+      render "new"
+    end
+  end
 end
