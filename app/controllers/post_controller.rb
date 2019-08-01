810a86f0f99a0c48ce8ee62e18d0977098fbd00b
Carlos Sol <fersolmarin@hotmail.com>
2019-08-01 12:17:23 -0500

    Creates Posts

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -5,12 +5,27 @@ class PostController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    if Post.create(post_params)
+      flash[:warning] = "Post Successful"
+      redirect_to post_path
+    else
+      flash.now[:warning] = "Post not saved, something went wrong"
+      render "new"
+    end
   end
 
-  private def checkSession
+  private
+  def checkSession
     redirect_to post_index_path unless sign_in?
+  end
+
+  def post_params
+    user = current_user
+    params[:post][:user_id] = user.id
+    params.require(:post).permit(:title, :content, :user_id)
   end
 end
