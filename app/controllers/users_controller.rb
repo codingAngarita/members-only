2493715709fabf6c99e6bb8cafa8dd85c2afc942
Carlos Sol <fersolmarin@hotmail.com>
2019-08-06 11:30:34 -0500

    Indentation Issue Fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
     end
   end
 
-  private def user_params
+  private
+  def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 end
